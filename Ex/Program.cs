37ab6627ae82da0cf94349cfca26f34d237e0629
Denis <antongetse@gmail.com>
2022-10-30 23:31:44 +0300

    Programm

--- Ex/Program.cs
+++ Ex/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = {"10", "Word", "yes"};
+
+PrintArray(array);
+Console.WriteLine();
+string[] array2 = GetNewArray(array);
+PrintArray(array2);
+
+string[] GetNewArray(string[] array)
+{ 
+    string[] arr = new string[array.Length];
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            arr[count] = array[i];
+            count++;
+        }
+    }
+    return arr;
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
